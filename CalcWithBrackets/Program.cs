96fccf65590b90e8a78f92e368cc7bed2d4e2c07
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-08-14 23:14:05 +1000

    AddCalcWithBrackets

---
+++ CalcWithBrackets/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc2
+{
+	internal class Program
+	{
+		static string expression;
+		static void Main(string[] args)
+		{
+			Console.Write("Введите арифметическое выражение: ");
+			//expression = Console.ReadLine();
+			expression = "(22+33)*(77-55)/11";
+			Console.WriteLine(expression);
+			Explore(expression);
+			Console.WriteLine(Calculate(expression));
+
+		}
+		static void Explore(string expression)
+		{
+			for (int i = 0; i < expression.Length; i++)
+			{
+				if (expression[i] == '(')
+				{
+					for (int j = i + 1; j < expression.Length; j++)
+					{
+						if (expression[j] == ')')
+						{
+							expression = expression.Replace
+								(
+								expression.Substring(i, j - i + 1), // подменяем часть выражения со скобками, на
+								Calculate(expression.Substring(i + 1, j - i - 1)).ToString()//в Calculate() передаём выражение без скобок
+								);
+							Program.expression = expression;
+
+						}
+						if (expression[j] == '(')
+						{
+							Explore(expression.Substring(j));
+							//Program.expression = expression; 
+						}
+					}
+				}
+			}
+		}
+		static double Calculate(string expression)
+		{
+
+
+			//char[] delimetrs = new char[] { '+', '-', '*', '/' };
+			string[] s_numbers = expression.Split('+', '-', '*', '/');
+			for (int i = 0; i < s_numbers.Length; i++) Console.Write(s_numbers[i] + "\t"); Console.WriteLine();
+			double[] numbers = new double[s_numbers.Length];
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				numbers[i] = Convert.ToDouble(s_numbers[i]);
+				Console.Write(numbers[i] + "\t");
+			}
+			Console.WriteLine();
+
+			//string[] operators = expression.Split('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '0');
+			string[] operators = expression.Split(s_numbers, StringSplitOptions.RemoveEmptyEntries);
+			for (int i = 0; i < operators.Length; i++) Console.Write(operators[i] + "\t"); Console.WriteLine();
+			//operators = operators.Where(val => val != "").ToArray();
+			for (int i = 0; i < operators.Length; i++) Console.Write(operators[i] + "\t"); Console.WriteLine();
+
+			for (int i = 0; i < operators.Length; i++)
+			{
+
+				while (operators[i] == "+" || operators[i] == "-")
+				{
+					if (operators[i] == "+") numbers[i] += numbers[i + 1];
+					if (operators[i] == "-") numbers[i] -= numbers[i + 1];
+					ShiftLeft(numbers, i + 1);
+					ShiftLeft(operators, i);
+
+				}
+			}
+			Print(numbers);
+			Print(operators);
+			return numbers[0];
+		}
+		static void Print(double[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i] + "\t");
+			}
+			Console.WriteLine();
+		}
+		static void Print(string[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i] + "\t");
+			}
+			Console.WriteLine();
+		}
+		static double[] ShiftLeft(double[] arr, int index = 0)
+		{
+			for (int i = index; i < arr.Length - 1; i++)
+				arr[i] = arr[i + 1];
+			arr[arr.Length - 1] = 0;
+			return arr;
+		}
+		static string[] ShiftLeft(string[] arr, int index = 0)
+		{
+			for (int i = index; i < arr.Length - 1; i++)
+				arr[i] = arr[i + 1];
+			arr[arr.Length - 1] = null;
+			return arr;
+		}
+	}
+}
